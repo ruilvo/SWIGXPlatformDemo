dec58bc5330e029eed4ef92691e1ece99d046d04
Rui Oliveira <ruimail24@gmail.com>
2021-01-30 19:15:32 +0000

    Add project files.

---
+++ SWIGXPlatformDemoExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SWIGXPlatformDemoExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var AC = new AmazingCounter();
+            Console.WriteLine($"Starting count: {AC.tick()}");
+            Console.WriteLine($"Another tick: {AC.tick()}");
+            Console.WriteLine($"Another tick: {AC.tick()}");
+            Console.WriteLine($"Another tick: {AC.tick()}");
+            Console.WriteLine($"Another tick: {AC.tick()}");
+            Console.WriteLine($"Another tick: {AC.tick()}");
+            Console.WriteLine($"Another tick: {AC.tick()}");
+            Console.WriteLine($"Another tick: {AC.tick()}");
+        }
+    }
+}
